60f13909daf4c72e0b528b52c14f7bb501245da8
ApurvaW18 <63594901+ApurvaW18@users.noreply.github.com>
2020-05-24 13:16:55 +0530

    Add files via upload

---
+++ Day1/Q4.py
@@ -0,0 +1,11 @@
+'''
+Question 4:
+
+Write a program to check wheather number is even or odd using if 
+Else statement…
+'''
+a=int(input())
+if a%2==0:
+    print("even")
+else:
+    print("odd")
---
+++ Day1/Q5.py
@@ -0,0 +1,14 @@
+'''
+Question 5:
+
+program that grants access only to kids aged between 8-12
+using  if  else statement
+Hint::
+If aged between 8 to 12 then you are allowed… welcome!!
+Otherwise Sorry not allowed ..Bye!
+'''
+a=int(input())
+if ((a>=8) and (a<=12)):
+    print("Welcome")
+else:
+    print("Bye")
